403e985e2b13759524a0ec78cce487cf15189bf1
Bert Loedeman <bert@loedeman.nl>
2017-10-09 23:24:39 +0200

    Started adding some XML documentation, bumped version number to publish new NuGet version.

--- DapperFilterExtensions/Data/Gateway.cs
+++ DapperFilterExtensions/Data/Gateway.cs
@@ -6,17 +6,24 @@ using DapperFilterExtensions.Filtering;
 
 namespace DapperFilterExtensions.Data
 {
+    /// <inheritdoc />
     public class Gateway<TData, TDataFilter> : IGateway<TData, TDataFilter> where TData: class where TDataFilter: DataFilter<TDataFilter, TData>
     {
         private readonly IDbConnectionFactory _connectionFactory;
         private readonly IPredicateFactory _predicateFactory;
 
+        /// <summary>
+        /// Creates a new <see cref="Gateway{TData, TDataFilter}"/> instance.
+        /// </summary>
+        /// <param name="connectionFactory">The <see cref="IDbConnectionFactory">connection factory</see> to use.</param>
+        /// <param name="predicateFactory">The <see cref="IPredicateFactory">predicate factory</see> to use.</param>
         public Gateway(IDbConnectionFactory connectionFactory, IPredicateFactory predicateFactory)
         {
             _connectionFactory = connectionFactory;
             _predicateFactory = predicateFactory;
         }
 
+        /// <inheritdoc />
         public async Task<IEnumerable<TData>> Get(TDataFilter filter)
         {
             var filterPredicate = _predicateFactory.GetPredicate<TDataFilter, TData>(filter);
@@ -24,23 +31,27 @@ namespace DapperFilterExtensions.Data
             return dataItems?.ToList();
         }
 
+        /// <inheritdoc />
         public async Task<TData> GetSingle(int id)
         {
             return await _connectionFactory.GetConnection().GetAsync<TData>(id);
         }
 
+        /// <inheritdoc />
         public Task<int> Add(TData model)
         {
             int id = _connectionFactory.GetConnection().Insert(model);
             return Task.FromResult(id);
         }
 
+        /// <inheritdoc />
         public Task<bool> Update(TData model)
         {
             var success = _connectionFactory.GetConnection().Update(model);
             return Task.FromResult(success);
         }
 
+        /// <inheritdoc />
         public Task<bool> Delete(TData speaker)
         {
             var success = _connectionFactory.GetConnection().Delete(speaker);
